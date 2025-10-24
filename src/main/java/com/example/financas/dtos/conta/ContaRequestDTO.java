8d403ceec250586e913655aa29a0914744b4dad6
Laís <laisluanaka@outlook.com>
2025-10-24 13:31:36 -0300

    continuação

--- src/main/java/com/example/financas/dtos/conta/ContaRequestDTO.java
+++ src/main/java/com/example/financas/dtos/conta/ContaRequestDTO.java
@@ -1,4 +1,21 @@
 package com.example.financas.dtos.conta;
 
-public record ContaRequestDTO() {
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record ContaRequestDTO(
+        @NotBlank(message = "O nome da conta é obrigatório")
+        @Size(min = 3, max = 50, message = "O nome da conta deve ter entre 3 e 50 caracteres")
+        String nome,
+
+        @NotBlank(message = "O tipo da conta é obrigatório")
+        @Size(min = 3, max = 50, message = "O tipo da conta deve ter entre 3 e 50 caracteres")
+        String tipo,
+
+        @PositiveOrZero(message = "O saldo inicial não pode ser negativo")
+        Double saldo
+) {
+
 }
--- src/main/java/com/example/financas/services/ContaService.java
+++ src/main/java/com/example/financas/services/ContaService.java
@@ -1,7 +1,16 @@
 package com.example.financas.services;
 
+import com.example.financas.dtos.conta.ContaResponseDTO;
+import com.example.financas.mappers.ContaMapper;
+import com.example.financas.models.Conta;
 import com.example.financas.repositories.RepositorioConta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ContaService {
@@ -11,6 +20,12 @@ public class ContaService {
             RepositorioConta repositorioConta
     ) {
         this.repositorioConta = repositorioConta;
+    }
+
+    public static List<ContaResponseDTO> getAllContas(int paginaAtual, int tamanhoPagina) {
+        Pageable pageable = PageRequest.of(paginaAtual, tamanhoPagina, Sort.by("nome"));
+        Page<Conta> page = repositorioConta.findAll(pageable);
+        return page.stream().map(ContaMapper::toDto).toList();
     }
 
 }

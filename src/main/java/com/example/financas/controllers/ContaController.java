8447f8a8f332dcef49b4b0c3bd392a9eb186e600
laiscasagrande <laisluanaka@outlook.com>
2025-10-26 17:51:00 -0300

    feat: controller e service do model Conta finalizados

--- src/main/java/com/example/financas/controllers/ContaController.java
+++ src/main/java/com/example/financas/controllers/ContaController.java
@@ -1,23 +1,50 @@
 package com.example.financas.controllers;
 
+import com.example.financas.dtos.conta.ContaRequestDTO;
 import com.example.financas.dtos.conta.ContaResponseDTO;
 import com.example.financas.services.ContaService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/contas")
 public class ContaController {
+
+    private final ContaService contaService;
+
+    public ContaController(ContaService contaService) {
+        this.contaService = contaService;
+    }
+
     @GetMapping
     public List<ContaResponseDTO> getAllContas(
             @RequestParam(defaultValue = "0") int paginaAtual,
             @RequestParam(defaultValue = "10") int tamanhoPagina
     ) {
-        return ContaService.getAllContas(paginaAtual, tamanhoPagina);
+        return contaService.getAllContas(paginaAtual, tamanhoPagina);
     }
 
+    @GetMapping("/{id}")
+    public ContaResponseDTO getContaPorId(@PathVariable UUID id){
+        return contaService.getContaPorId(id);
+    }
+
+    @PostMapping
+    public ContaResponseDTO createConta(@RequestBody ContaRequestDTO contaRequestDTO){
+        return contaService.createConta(contaRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ContaResponseDTO updateConta(@PathVariable UUID id, @RequestBody ContaRequestDTO contaRequestDTO) {
+        return contaService.updateConta(id, contaRequestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteConta(@PathVariable UUID id) {
+        contaService.deleteConta(id);
+    }
 }
+
+
--- src/main/java/com/example/financas/services/ContaService.java
+++ src/main/java/com/example/financas/services/ContaService.java
@@ -1,5 +1,6 @@
 package com.example.financas.services;
 
+import com.example.financas.dtos.conta.ContaRequestDTO;
 import com.example.financas.dtos.conta.ContaResponseDTO;
 import com.example.financas.mappers.ContaMapper;
 import com.example.financas.models.Conta;
@@ -11,10 +12,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ContaService {
     private final RepositorioConta repositorioConta;
+    private ContaMapper contaMapper;
 
     public ContaService(
             RepositorioConta repositorioConta
@@ -22,10 +25,45 @@ public class ContaService {
         this.repositorioConta = repositorioConta;
     }
 
-    public static List<ContaResponseDTO> getAllContas(int paginaAtual, int tamanhoPagina) {
+    public List<ContaResponseDTO> getAllContas(int paginaAtual, int tamanhoPagina) {
         Pageable pageable = PageRequest.of(paginaAtual, tamanhoPagina, Sort.by("nome"));
         Page<Conta> page = repositorioConta.findAll(pageable);
         return page.stream().map(ContaMapper::toDto).toList();
     }
 
+    public ContaResponseDTO getContaPorId(UUID id) {
+        Conta conta = repositorioConta.findById(id)
+                .orElseThrow(() -> new RuntimeException("Conta não encontrada com o ID: " + id));
+        return contaMapper.toDto(conta);
+    }
+
+    public ContaResponseDTO createConta(ContaRequestDTO dto) {
+        Conta conta = new Conta();
+        conta.setNome(dto.nome());
+        conta.setTipo(dto.tipo());
+        conta.setSaldo(dto.saldo());
+        conta.setCriadoEm(java.time.LocalDateTime.now());
+        conta.setAtualizadoEm(java.time.LocalDateTime.now());
+        Conta save = repositorioConta.save(conta);
+        return contaMapper.toDto(save);
+    }
+
+    public ContaResponseDTO updateConta(UUID id, ContaRequestDTO dto) {
+        Conta conta = repositorioConta.findById(id)
+                .orElseThrow(() -> new RuntimeException("Conta não encontrada com o ID: " + id));
+
+        conta.setNome(dto.nome());
+        conta.setSaldo(dto.saldo());
+        conta.setAtualizadoEm(java.time.LocalDateTime.now());
+
+        Conta updated = repositorioConta.save(conta);
+        return contaMapper.toDto(updated);
+    }
+
+    public void deleteConta(UUID id) {
+        if (!repositorioConta.existsById(id)) {
+            throw new RuntimeException("Conta não encontrada com o ID: " + id);
+        }
+        repositorioConta.deleteById(id);
+    }
 }

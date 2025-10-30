aaf97459b8b44aeba6ad5e74520f20dad1cadef0
Laís <laisluanaka@outlook.com>
2025-10-30 12:32:47 -0300

    continuação de Transacao

--- src/main/java/com/example/financas/controllers/TransacaoController.java
+++ src/main/java/com/example/financas/controllers/TransacaoController.java
@@ -1,5 +1,7 @@
 package com.example.financas.controllers;
 
+import com.example.financas.dtos.conta.ContaRequestDTO;
+import com.example.financas.dtos.conta.ContaResponseDTO;
 import com.example.financas.dtos.transacao.TransacaoRequestDTO;
 import com.example.financas.dtos.transacao.TransacaoResponseDTO;
 import com.example.financas.services.TransacaoService;
@@ -7,6 +9,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transacoes")
@@ -25,8 +28,19 @@ public class TransacaoController {
         return transacaoService.getAllTransacoes(paginaAtual, tamanhoPagina);
     }
 
+    @GetMapping("/{id}")
+    public TransacaoResponseDTO getTransacaoPorId(@PathVariable UUID id){
+        return transacaoService.getTransacaoPorId(id);
+    }
+
     @PostMapping
     public TransacaoResponseDTO criar(@RequestBody @Valid TransacaoRequestDTO dto) {
-        return transacaoService.criar(dto);
+        return transacaoService.createTransacao(dto);
     }
+
+    @PutMapping("/{id}")
+    public TransacaoResponseDTO updateTransacao(@PathVariable UUID id, @RequestBody TransacaoRequestDTO dto) {
+        return transacaoService.updateTransacao(id, dto);
+    }
+
 }
--- src/main/java/com/example/financas/services/TransacaoService.java
+++ src/main/java/com/example/financas/services/TransacaoService.java
@@ -1,7 +1,9 @@
 package com.example.financas.services;
-
+import com.example.financas.dtos.transacao.TransacaoRequestDTO;
 import com.example.financas.dtos.transacao.TransacaoResponseDTO;
+import com.example.financas.mappers.ContaMapper;
 import com.example.financas.mappers.TransacaoMapper;
+import com.example.financas.models.Conta;
 import com.example.financas.models.Transacao;
 import com.example.financas.repositories.RepositorioTransacao;
 import org.springframework.data.domain.Page;
@@ -11,10 +13,12 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransacaoService {
     private final RepositorioTransacao repositorioTransacao;
+    private TransacaoMapper transacaoMapper;
 
     public TransacaoService(RepositorioTransacao repositorioTransacao) {
         this.repositorioTransacao = repositorioTransacao;
@@ -24,5 +28,24 @@ public class TransacaoService {
         Pageable pageable = PageRequest.of(paginaAtual, tamanhoPagina, Sort.by("criadoEm").descending());
         Page<Transacao> page = repositorioTransacao.findAll(pageable);
         return page.stream().map(TransacaoMapper::toDto).toList();
+    }
+
+    public TransacaoResponseDTO getTransacaoPorId(UUID id) {
+        Transacao transacao = repositorioTransacao.findById(id)
+                .orElseThrow(() -> new RuntimeException("Transação não encontrada com o ID: " + id));
+        return transacaoMapper.toDto(transacao);
+    }
+
+    public TransacaoResponseDTO createTransacao(TransacaoRequestDTO dto) {
+        Transacao transacao = new Transacao();
+        transacao.setUsuarioId(dto.usuarioId());
+        transacao.setId(dto.contaId());
+        transacao.setCategoriaId(dto.categoriaId());
+        transacao.setTipo(dto.tipo());
+        transacao.setValor(dto.valor());
+        transacao.setData(java.time.LocalDateTime.now());
+        transacao.setDescricao(dto.descricao());
+        Transacao save = repositorioTransacao.save(transacao);
+        return transacaoMapper.toDto(save);
     }
 }

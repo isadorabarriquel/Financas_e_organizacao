c44040930523ca4c17d909cdbad51fc9e89306dc
FernandoTomasi <fernando.279816@alunosatc.edu.br>
2025-10-31 13:47:24 -0300

    Correção das anotações @NotBlank para @NotNull nos DTOs

--- src/main/java/com/example/financas/dtos/conta/ContaRequestDTO.java
+++ src/main/java/com/example/financas/dtos/conta/ContaRequestDTO.java
@@ -1,11 +1,12 @@
 package com.example.financas.dtos.conta;
 
-
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 
 public record ContaRequestDTO(
+
         @NotBlank(message = "O nome da conta é obrigatório")
         @Size(min = 3, max = 50, message = "O nome da conta deve ter entre 3 e 50 caracteres")
         String nome,
@@ -14,8 +15,7 @@ public record ContaRequestDTO(
         @Size(min = 3, max = 50, message = "O tipo da conta deve ter entre 3 e 50 caracteres")
         String tipo,
 
+        @NotNull(message = "O saldo inicial é obrigatório")
         @PositiveOrZero(message = "O saldo inicial não pode ser negativo")
         Double saldo
-) {
-
-}
+) {}
--- src/main/java/com/example/financas/dtos/transacao/TransacaoRequestDTO.java
+++ src/main/java/com/example/financas/dtos/transacao/TransacaoRequestDTO.java
@@ -1,21 +1,23 @@
 package com.example.financas.dtos.transacao;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record TransacaoRequestDTO(
-        @NotBlank(message = "Usuário não pode ser nulo")
+
+        @NotNull(message = "Usuário não pode ser nulo")
         UUID usuarioId,
 
-        @NotBlank(message = "Conta não pode ser nula")
+        @NotNull(message = "Conta não pode ser nula")
         UUID contaId,
 
-        @NotBlank(message = "A categoria é obrigatória")
+        @NotNull(message = "A categoria é obrigatória")
         UUID categoriaId,
 
         @NotBlank(message = "Tipo de transação obrigatório")
@@ -23,14 +25,13 @@ public record TransacaoRequestDTO(
         @Pattern(regexp = "RECEITA|DESPESA|TRANSFERENCIA", message = "O tipo deve ser RECEITA, DESPESA ou TRANSFERENCIA")
         String tipo,
 
-        @NotBlank(message = "Valor é obrigatório")
-        @Positive(message = "valor deve ser positivo")
+        @NotNull(message = "Valor é obrigatório")
+        @Positive(message = "Valor deve ser positivo")
         Double valor,
 
-        @NotBlank (message = "A data é obrigatória")
+        @NotNull(message = "A data é obrigatória")
         LocalDateTime data,
 
         @Size(max = 100, message = "A descrição pode ter no máximo 100 caracteres")
         String descricao
-) {
-}
+) {}

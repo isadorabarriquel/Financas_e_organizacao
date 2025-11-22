e2c55d9227585642672954566b8a37594cc52367
Isadora Barriquel <isadorabarriquel4@gmail.com>
2025-11-22 14:59:50 -0300

    Adicionando restante do service de usuário

--- src/main/java/com/example/financas/controllers/UsuarioController.java
+++ src/main/java/com/example/financas/controllers/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.example.financas.controllers;
 
+import com.example.financas.dtos.usuario.UsuarioRequestDTO;
 import com.example.financas.dtos.usuario.UsuarioResponseDTO;
 import com.example.financas.services.UsuarioService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -19,8 +21,31 @@ public class UsuarioController {
     @GetMapping
     public List<UsuarioResponseDTO> getAllUsuarios(
             @RequestParam(defaultValue = "0") int paginaAtual,
-            @RequestParam(defaultValue = "10") int tamanhoPagina
+            @RequestParam(defaultValue = "10") int tamanhoPagina,
+            @RequestParam(required = false) String nome
     ) {
-        return usuarioService.getAllUsuarios(paginaAtual, tamanhoPagina);
+        return usuarioService.getAllUsuarios(paginaAtual, tamanhoPagina, nome);
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioResponseDTO getUsuarioPorId(@PathVariable UUID id) {
+        return usuarioService.getUsuarioPorId(id);
+    }
+
+    @PostMapping
+    public UsuarioResponseDTO createUsuario(@RequestBody UsuarioRequestDTO dto) {
+        return usuarioService.createUsuario(dto);
+    }
+
+    public UsuarioResponseDTO updateUsuario(
+            @PathVariable UUID id,
+            @RequestBody UsuarioRequestDTO dto
+    ) {
+        return usuarioService.updateUsuario(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable UUID id) {
+        usuarioService.deleteUsuario(id);
     }
 }
--- src/main/java/com/example/financas/services/UsuarioService.java
+++ src/main/java/com/example/financas/services/UsuarioService.java
@@ -1,5 +1,6 @@
 package com.example.financas.services;
 
+import com.example.financas.dtos.usuario.UsuarioRequestDTO;
 import com.example.financas.dtos.usuario.UsuarioResponseDTO;
 import com.example.financas.mappers.UsuarioMapper;
 import com.example.financas.models.Usuario;
@@ -10,7 +11,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 public class UsuarioService {
@@ -20,9 +24,61 @@ public class UsuarioService {
         this.repositorioUsuario = repositorioUsuario;
     }
 
-    public List<UsuarioResponseDTO> getAllUsuarios(int paginaAtual, int tamanhoPagina) {
-        Pageable pageable = PageRequest.of(paginaAtual, tamanhoPagina, Sort.by("nome"));
+    public List<UsuarioResponseDTO> getAllUsuarios(
+            int paginaAtual,
+            int tamanhoPagina,
+            String nome) {
+        Pageable pageable = PageRequest.of(paginaAtual, tamanhoPagina, Sort.by("nome").ascending());
         Page<Usuario> page = repositorioUsuario.findAll(pageable);
-        return page.stream().map(UsuarioMapper::toDto).toList();
+        /*return page.stream().map(UsuarioMapper::toDto).toList();*/
+
+        Stream<Usuario> stream = page.stream();
+
+        if (nome != null && !nome.isBlank()) {
+            String nomeUpper = nome.toUpperCase();
+            stream = stream.filter(u -> u.getNome() != null &&
+                    u.getNome().toUpperCase().contains(nomeUpper));
+        }
+        return stream
+                .map(UsuarioMapper::toDto)
+                .toList();
+    }
+
+    public UsuarioResponseDTO getUsuarioPorId(UUID id) {
+        Usuario usuario = repositorioUsuario.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário " + id +" não encontrado"));
+
+        return UsuarioMapper.toDto(usuario);
+    }
+
+    public UsuarioResponseDTO createUsuario(UsuarioRequestDTO dto) {
+        Usuario usuario = new Usuario();
+
+        usuario.setNome(dto.nome());
+        usuario.setEmail(dto.email());
+        usuario.setCriadoEm(LocalDateTime.now());
+        usuario.setAtualizadoEm(LocalDateTime.now());
+
+        Usuario salvo = repositorioUsuario.save(usuario);
+        return UsuarioMapper.toDto(salvo);
+    }
+
+    public UsuarioResponseDTO updateUsuario(UUID id, UsuarioRequestDTO dto) {
+        Usuario usuario = repositorioUsuario.findById(id)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
+
+        usuario.setNome(dto.nome());
+        usuario.setEmail(dto.email());
+        usuario.setAtualizadoEm(LocalDateTime.now());
+
+        Usuario atualizado = repositorioUsuario.save(usuario);
+        return UsuarioMapper.toDto(atualizado);
+    }
+
+    public void deleteUsuario(UUID id) {
+        if (!repositorioUsuario.existsById(id)) {
+            throw new RuntimeException("Usuário não encontrado com o ID: " + id);
+        }
+        repositorioUsuario.deleteById(id);
     }
 }

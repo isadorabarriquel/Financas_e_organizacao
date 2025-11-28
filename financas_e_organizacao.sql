e7d111d44bef596fb905c95a87f1a401ecb3f434
Isadora Barriquel <isadorabarriquel4@gmail.com>
2025-11-28 20:54:02 -0300

    atualizando banco de dados

--- financas_e_organizacao.sql
+++ financas_e_organizacao.sql
@@ -1,46 +1,19 @@
-create table usuario(
-	id integer not null primary key,
-	nome varchar(50) not null,
-	email varchar(100) not null,
-	senha varchar(16) not null,
-	dt_criacao date,
-	dt_atualizacao date
-);
+-- Table: public.usuario
 
-create table conta(
-	id integer not null primary key,
-	id_usuario integer,
-	nome varchar(20),
-	tipo varchar(20),
-	moeda varchar(15),
-	saldo numeric(10,2),
-	dt_criacao date, 
-	dt_atualizacao date,
-	constraint fk_id_usuario_conta foreign key (id_usuario) references usuario(id)
-);
+-- DROP TABLE IF EXISTS public.usuario;
 
-create table categoria(
-	id integer not null primary key,
-	id_usuario integer, 
-	nome varchar(20) not null,
-	tipo varchar(20),
-	dt_criacao date,
-	dt_atualizacao date,
-	constraint fk_id_usuario_categoria foreign key (id_usuario) references usuario(id)
-);
+CREATE TABLE IF NOT EXISTS public.usuario
+(
+    id integer NOT NULL,
+    nome character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    senha character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    dt_criacao date,
+    dt_atualizacao date,
+    CONSTRAINT usuario_pkey PRIMARY KEY (id)
+)
 
-create table transacao(
-	id integer not null primary key,
-	id_usuario integer,
-	id_conta integer, 
-	id_categoria integer,
-	tipo varchar(20),
-	valor numeric(10,2),
-	dt_transacao date, 
-	descricao varchar(50),
-	dt_criacao date,
-	dt_atualizacao date,
-	constraint fk_id_usuario_transacao foreign key (id_usuario) references usuario(id),
-	constraint fk_id_conta_transacao foreign key (id_conta) references conta(id),
-	constraint fk_id_categoria_transacao foreign key (id_categoria) references categoria(id)
-);
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.usuario
+    OWNER to postgres;
